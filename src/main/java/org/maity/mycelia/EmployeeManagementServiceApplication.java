099cc1337e3074d2d0ae38a8661f2d67f7df30c1
maskysans <dolanmaity96@gmail.com>
2025-09-01 00:58:17 +0530

    Initial Commit

---
+++ src/main/java/org/maity/mycelia/EmployeeManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package org.maity.mycelia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/maity/mycelia/EmployeeManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maity.mycelia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
